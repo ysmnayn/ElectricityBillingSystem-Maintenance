66eb5f6f81d3da34490bed7408530af9c6c0bc40
Hafiy <hafiyazfar05@gmail.com>
2026-01-03 16:21:15 +0800

    add comment

--- SignUp.java
+++ SignUp.java
@@ -8,7 +8,7 @@ import java.sql.*;
 
 public class SignUp extends JFrame implements ActionListener {
 
-    JButton Create, back;
+    JButton Create, back; //commented by hafiy
     Choice create;
     JTextField uname, namee, pd, meter;
 
