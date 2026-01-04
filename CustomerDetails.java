1bbb7c6de5780a683be2be550f60d77d3670f207
Yasmin Ayuni <ysmnayn04@gmail.com>
2026-01-04 19:47:45 +0800

    Update location value

--- CustomerDetails.java
+++ CustomerDetails.java
@@ -16,8 +16,7 @@ public class CustomerDetails extends JFrame implements ActionListener{
         super("Customer Details");
         
         setSize(1200,650); // Comment by Yasmin - testing Git collaboration
-        setLocation(300,150); // Testing merge conflicts 
-       
+        setLocation(400,150); // changed by yasmin
         
         table = new JTable();
         
